6aa8102f24cf0e924b272ec89eb7532fe02f775c
Lewis Wilkins <lewis.wilkins@cern.ch>
2019-08-20 22:27:39 +0100

    Few minor fixes

--- ArrayTomographyLib/Colocalisation.py
+++ ArrayTomographyLib/Colocalisation.py
@@ -24,21 +24,20 @@ class Colocalisation(object):
         self.channels_arr = channels_arr
     
     
-    def fill_distance_jit(channels):
-        for channel_i in self.channels_arr:
+    def run_distance(self):
+        for i,channel_i in enumerate(self.channels_arr):
             print("Comparing {0} with all other channels.".format(channel_i.name))
-            for centroid_i in channels_i.centroids:
+            for ii,centroid_i in enumerate(channels_i.centroids):
                 for channel_j in self.channels_arr:
                     centroids_j = channel_j.centroids
-                    distance = self._get_best_distance(centroid_i, centroids_j)
-                    if distance < 0.5:
-                        channels[channel_i]["distance"] = distance
-            end = time.time()
-            print("time = {0}s, iterations = {1}".format(end-start, iterations))
+                    distance = self._get_best_distance(centroid_i, centroids_j,
+                                            self.xy_resolution, self.z_resolution)
+                    if distance < self.max_distance:
+                        self.results_arr[i].colocalisation(ii)[channel_j] = 1
 
 
     @njit(cache=True, fastmath=True)
-    def _get_best_distance(centroids, centroids_list, xy_resolution=0.102,
+    def _get_best_distance(self,centroids, centroids_list, xy_resolution=0.102,
                             z_resolution=0.07):
         best_distance = 9999
         for i in range(centroids_j.shape[0]):
