94a77b1c0cde0978b2552af92c2b8222577eb658
Lewis Wilkins <lewis.wilkins@cern.ch>
2019-08-20 22:28:19 +0100

    Adding test code

---
+++ test/test_ColocalisationSave.py
@@ -0,0 +1,17 @@
+from ArrayTomographyLib import ColocalisationSave
+import numpy as np 
+
+if __name__ == "__main__":
+    
+    n_objects = 10
+    colocalisation_save = ColocalisationSave.ColocalisationSave("A",10, ["B", "C"])
+    for a in range(n_objects):
+        colocalisation_save.colocalisations[a] = {"A": 1,
+                                                "B": np.random.randint(2),
+                                                "C": np.random.randint(2),
+                            
+                                       }
+    
+    colocalisation_save.print_colocalisations()
+    colocalisation_save.get_results()
+    # colocalisa/tion_save.get_combinations()
