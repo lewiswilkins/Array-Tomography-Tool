c83f6d88d4c39573a17b64a4afe637d7bbaedbe9
Lewis Wilkins <lewis.wilkins@cern.ch>
2019-09-01 15:16:05 +0100

    Fully implemented overlap calculation. Also minor changes to ChannelFile

--- ArrayTomographyLib/Colocalisation.py
+++ ArrayTomographyLib/Colocalisation.py
@@ -24,6 +24,7 @@ class Colocalisation(object):
         self.channels_arr = channels_arr
     
     
+    #Runs the colocalisation code. Two options are distacne or overlap
     def run_colocalisation(self):
         for i,channel_i in enumerate(self.channels_arr):
             print("Comparing {0} with all other channels.".format(channel_i.name))
@@ -31,21 +32,28 @@ class Colocalisation(object):
                 for channel_j in self.channels_arr:
                     if channel_i.colocalisation_type[channel_j.name] == "distance":
                         centroid_i = self.channels_arr.centroids[ii]
-                        self._run_distance(centroid_i, channel_j)
+                        self.results_arr[i].colocalisation(ii)[channel_j] = self._run_distance(centroid_i, channel_j)
                     elif channel_i.colocalisation_type[channel_j.name] == "overlap":
+                        coord_i = self.channels_arr.pixel_list[ii]
+                        self.results_arr[i].colocalisation(ii)[channel_j] = self._run_overlap()
                         pass
+
 
     def _run_distance(self, centroid_i, channel_j):
                 centroids_j = selchannel_j.centroids
-                distance = self._get_best_distance(centroid_i, centroids_j,
-                                        self.xy_resolution, self.z_resolution)
-                if distance < self.max_distance:
-                    self.results_arr[i].colocalisation(ii)[channel_j] = 1
+        distance = self._get_best_distance(centroid_i, centroids_j,
+                                self.xy_resolution, self.z_resolution)
+        if distance < self.max_distance:
+            return 1
+        else:
+            return 0
+            
 
     @njit(cache=True, fastmath=True)
     def _get_best_distance(self,centroids, centroids_list, xy_resolution=0.102,
                             z_resolution=0.07):
         best_distance = 9999
+        #Loop over all centroids in channel j and returns the closest distance
         for i in range(centroids_j.shape[0]):
             distance = sqrt(pow((centroids[0]-centroids_list[i][0])*xy_resolution,2)+
                             pow((centroids[1]-centroids_list[i][1])*xy_resolution,2)+
@@ -56,12 +64,17 @@ class Colocalisation(object):
         return best_distance
 
 
-    def _run_overlap(self):
-        pass
+    def _run_overlap(self, coords_i, channel_j):
+        coords_list_j = channel_j.pixel_list
+        overlap = self._get_overlap(coords_i, coords_list_j, self.index_location)
+        if (overlap/len(coords_i))*100 > self.min_overlap:
+            return 1
+        else:
+            return 0
     
-    
-    @njit
-    def _get_best_overlap(self, coords_i, coords_list_j, index_location):
+
+   @njit(cache=True)
+    def _get_overlap(self, coords_i, coords_list_j, index_location):
         previous_index = 0
         overlap = 0
         #loop over the index locations - basically loop over objects
@@ -70,19 +83,24 @@ class Colocalisation(object):
             coordiante_j = coords_list_j[previous_index:index] #get the section of array which is curen obj
             coordiante_length = int(len(coordiante_j)/3) 
             coordiante_j_reshape = coordiante_j.reshape((coordiante_length, 3))#reshape to original
-            
             #now check how many overlap - doing manually because intersect1d is not avaiblable in numba
             n_overlapping = 0
             for ii in range(len(coords_i)):
                 for jj in range(ii, len(coordiante_j_reshape)):
-                    print(coords_i[ii],coordiante_j_reshape[jj]) 
-    #                 if coords_i[ii] == coordiante_j_reshape[jj]:
-    #                     n_overlapping+=1
+                    for k in range(3):
+                        if coords_i[ii][k] == coordiante_j_reshape[jj][k]:
+                            match = True
+                        else:
+                            match = False
+                    if match:
+                        n_overlapping+=1     
             #what we really care about is the max number of overlaps between any combination
             if n_overlapping > overlap:
                 overlap = n_overlapping
             previous_index = index
-    #   
         #return the max
         return overlap
-            pass
+   
+
+
+
