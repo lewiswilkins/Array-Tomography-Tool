d5ac64906efe250fd78a21d7976e4ceeae4563f3
Lewis Wilkins <lewis.wilkins@cern.ch>
2019-09-01 22:28:58 +0100

    Working version of process_stacks

--- process_stacks.py
+++ process_stacks.py
@@ -1,7 +1,32 @@
 from ArrayTomographyLib import ChannelFile
 import glob
+import os
+
+
+def check_dir_exists(dir_path):
+    if not os.path.isdir(dir_path):
+        print("Directory does not exist. Creating now!")
+        os.mkdir(dir_path)
 
 if __name__ == "__main__":
-    print("Processing image stacks")
+    print("Processing image stacks..")
     #File list to be processed, what options, output to pickle
     #Do we glob a dir? need output dir etc.
+    in_dir = input("Directory containing imgage stacks: ")
+    out_dir = input("Output directory to store pickle files: ")
+    check_dir_exists(out_dir)
+
+    image_stacks = glob.glob("{0}/*.tif".format(in_dir))
+
+    for stack in image_stacks:
+        stack_name = stack.split("/")[-1].split('.')[0]
+        print("Processing {0}.".format(stack_name))
+        channel_file = ChannelFile.ChannelFile(name=stack_name,
+                                               file_name=stack)
+        channel_file.get_lables()
+        channel_file.get_regionprops()
+        channel_file.get_centroids()
+        channel_file.get_pixel_list()
+
+        channel_file.save_to_pickle(file_name="{0}/{1}.pickle".format(out_dir, stack_name))
+    
