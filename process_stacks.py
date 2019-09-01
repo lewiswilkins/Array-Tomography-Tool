48e83e76d61ab93db8d1b4212d83e00995278dab
Lewis Wilkins <lewis.wilkins@cern.ch>
2019-09-01 16:04:58 +0100

    Adding image processing and colocalisation file skeletons.

---
+++ process_stacks.py
@@ -0,0 +1,7 @@
+from ArrayTomographyLib import ChannelFile
+import glob
+
+if __name__ == "__main__":
+    print("Processing image stacks")
+    #File list to be processed, what options, output to pickle
+    #Do we glob a dir? need output dir etc.
