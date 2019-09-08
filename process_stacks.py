2ffd693a3a0aa14eb4ae330cbc0abecbd57dcb11
Alex Persin <apersin11@gmail.com>
2019-09-08 14:56:23 +0100

    conforms to pep8, adds f strings

--- process_stacks.py
+++ process_stacks.py
@@ -8,25 +8,24 @@ def check_dir_exists(dir_path):
         print("Directory does not exist. Creating now!")
         os.mkdir(dir_path)
 
+
 if __name__ == "__main__":
     print("Processing image stacks..")
-    #File list to be processed, what options, output to pickle
-    #Do we glob a dir? need output dir etc.
+    # File list to be processed, what options, output to pickle
+    # Do we glob a dir? need output dir etc.
     in_dir = input("Directory containing imgage stacks: ")
     out_dir = input("Output directory to store pickle files: ")
     check_dir_exists(out_dir)
-    
-    image_stacks = glob.glob("{0}/*.tif".format(in_dir))
+
+    image_stacks = glob.glob(f"{in_dir}/*.tif")
 
     for stack in image_stacks:
-        stack_name = stack.split("/")[-1].split('.')[0]
-        print("Processing {0}.".format(stack_name))
-        channel_file = ChannelFile.ChannelFile(name=stack_name,
-                                               file_name=stack)
+        stack_name = stack.split("/")[-1].split(".")[0]
+        print(f"Processing {stack_name}.")
+        channel_file = ChannelFile.ChannelFile(name=stack_name, file_name=stack)
         channel_file.get_lables()
         channel_file.get_regionprops()
         channel_file.get_centroids()
         channel_file.get_pixel_list()
 
-        channel_file.save_to_pickle(file_name="{0}/{1}.pickle".format(out_dir, stack_name))
-    
+        channel_file.save_to_pickle(file_name=f"{out_dir}/{stack_name}.pickle")
