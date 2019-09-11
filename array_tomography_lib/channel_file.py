515fb5296f4c0ad61b7127a48d1dc18ac7eebaff
Alex Persin <apersin11@gmail.com>
2019-09-11 23:57:10 +0100

    fixes to tool

--- array_tomography_lib/channel_file.py
+++ array_tomography_lib/channel_file.py
@@ -27,11 +27,7 @@ class ChannelFile:
 
     def load_tiff(self, file_name, plugin="pil"):
         self.file_name = file_name
-        try:
-            self.image = np.array(io.imread(file_name, plugin=plugin))
-        except FileNotFoundError:
-            print("File not found!")
-            sys.exit()
+        self.image = np.array(io.imread(file_name, plugin=plugin))
 
     def get_lables(self, opt=1):
         self.image_labels = measure.label(self.image, connectivity=opt)
@@ -40,37 +36,31 @@ class ChannelFile:
         try:
             self.image_regionprops = measure.regionprops(self.image_labels)
         except AttributeError:
-            print("No labels present! Please call get_labels first.")
-            sys.exit()
+            self.get_labels()
+            self.get_regionprops()
 
     def get_centroids(self):
         try:
             self.centroids = np.array([x.centroid for x in self.image_regionprops])
         except AttributeError:
-            print("No regionprops present! Please call get_regionprops first.")
-            sys.exit()
+            self.get_regionprops()
+            self.get_centroids()
 
     def get_pixel_list(self):
         try:
             self.pixel_list = np.array([np.array(x.coords) for x in self.image_regionprops])
-            # self.pixel_list_flat = self._get_flat_array(self.pixel_list)
-            # self.pixel_list_index = np.array([len(x.flatten()) for x in self.pixel_list])
         except AttributeError:
-            print("No regionprops present! Please call get_regionprops first.")
-            sys.exit()
+            self.get_regionprops()
+            self.get_pixel_list()
 
     def save_to_pickle(self, file_name):
         with open(file_name, "wb") as output_pickle:
             pickle.dump(self, output_pickle)
 
     def load_from_pickle(self, file_name):
-        try:
-            with open(file_name, "rb") as input_pickle:
-                self = pickle.load(input_pickle)
-            return self
-        except FileNotFoundError:
-            print(f"{file_name} does not exist. Please try another file name.")
-            sys.exit()
+        with open(file_name, "rb") as input_pickle:
+            self = pickle.load(input_pickle)
+        return self
 
     def set_colocalisation_types(self, colocalisation_types):
         self.colocalisation_types = colocalisation_types
